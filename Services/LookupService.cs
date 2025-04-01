a8d50d81ee19c7edb3a75420f52a4ff4666a0c9d
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-01 23:13:45 +0200

    add service

---
+++ Services/LookupService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using socialAssistanceFundMIS.Data;
+using socialAssistanceFundMIS.Models;
+
+namespace SocialAssistanceFundMisMcv.Services
+{
+    public class LookupService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LookupService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AssistanceProgram>> GetProgramsAsync()
+        {
+            return await _context.AssistancePrograms.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Sex>> GetSexesAsync()
+        {
+            return await _context.Sexes.ToListAsync();
+        }
+
+        public async Task<IEnumerable<MaritalStatus>> GetMaritalStatusesAsync()
+        {
+            return await _context.MaritalStatuses.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Status>> GetStatusesAsync()
+        {
+            return await _context.Statuses.ToListAsync();
+        }
+    }
+}
