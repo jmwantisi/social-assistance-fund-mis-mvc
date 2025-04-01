5653f8722520861e4f9324c6d39d4863b996153e
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-01 23:11:58 +0200

    Add model

---
+++ Models/PhoneNumberType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace socialAssistanceFundMIS.Models
+{
+    public class PhoneNumberType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public bool Removed { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+
+}
