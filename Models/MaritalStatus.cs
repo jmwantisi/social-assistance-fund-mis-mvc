675c2a9ee821b0f1901342251a84f08eeb5b8c94
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-01 23:12:19 +0200

    Add model

---
+++ Models/MaritalStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace socialAssistanceFundMIS.Models
+{
+    public class MaritalStatus
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public bool Removed { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
