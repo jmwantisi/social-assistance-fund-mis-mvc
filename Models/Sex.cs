2f70636ae2a54e7960c7387f45911117cb7a07c2
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-01 23:11:51 +0200

    Add model

---
+++ Models/Sex.cs
@@ -0,0 +1,15 @@
+﻿namespace socialAssistanceFundMIS.Models
+{
+    public class Sex
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool Removed { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
