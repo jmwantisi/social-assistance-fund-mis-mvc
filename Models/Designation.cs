e6a1ff6143f175f57d32f67f1e6b0a270960c52d
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-01 23:12:37 +0200

    Add model

---
+++ Models/Designation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace socialAssistanceFundMIS.Models
+{
+    public class Designation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool Removed { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
