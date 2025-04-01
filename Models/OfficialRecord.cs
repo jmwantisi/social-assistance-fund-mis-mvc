01850a1dfe2ad0911295e201f667e0b7f1cfaaf8
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-01 23:12:04 +0200

    Add model

---
+++ Models/OfficialRecord.cs
@@ -0,0 +1,26 @@
+﻿using socialAssistanceFundMIS.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace socialAssistanceFundMIS.Models
+{
+    public class OfficialRecord
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OfficerId { get; set; }
+        public Officer? Officer { get; set; }
+
+        [Required]
+        public DateTime OfficiationDate { get; set; }
+
+        public bool Removed { get; set; } = false; // Using boolean for clarity
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime UpdatedAt { get; set; } // Consider updating this manually
+    }
+}
