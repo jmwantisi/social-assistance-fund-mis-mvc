d99b1d2999069f310c1d29aca742cf8a34b36d78
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-01 23:11:35 +0200

    Add model

---
+++ Models/ApplicantPhoneNumber.cs
@@ -0,0 +1,31 @@
+﻿using socialAssistanceFundMIS.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace socialAssistanceFundMIS.Models
+{
+    public class ApplicantPhoneNumber
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ApplicantId { get; set; }
+        public Applicant? Applicant { get; set; }
+
+        [Required]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        public int PhoneNumberTypeId { get; set; }
+
+        public PhoneNumberType? PhoneNumberType { get; set; }
+
+        public bool Removed { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+
+}
