5782d06dc2258ca90b65637d7c6040add192c176
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-01 23:12:43 +0200

    Add model

---
+++ Models/AssistanceProgram.cs
@@ -0,0 +1,16 @@
+﻿namespace socialAssistanceFundMIS.Models
+{
+    public class AssistanceProgram
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool Removed { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+
+}
