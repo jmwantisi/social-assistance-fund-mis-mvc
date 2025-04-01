484b6c4dd9c663c08fce1fbec9a759a57b88151b
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-01 23:12:12 +0200

    Add model

---
+++ Models/Officer.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace socialAssistanceFundMIS.Models
+{
+    public class Officer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? MiddleName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public int DesignationId { get; set; }
+        public Designation? Designation { get; set; }
+
+        public bool Removed { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; }
+    }
+
+}
