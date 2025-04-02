8c3881f0e68f7ec0ed1f2b3b7aa17566eb2d2361
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-03 00:40:10 +0200

    refactor application view model

--- ViewModels/ApplicationViewModel.cs
+++ ViewModels/ApplicationViewModel.cs
@@ -12,28 +12,27 @@ namespace SocialAssistanceFundMisMcv.ViewModels
         [Display(Name = "Application Date")]
         public DateTime ApplicationDate { get; set; }
 
+        public string? ApplicantFullName { get; set; }
+
         [Required]
         [Display(Name = "Applicant")]
-        public int applicantId { get; set; }
-        public IEnumerable<Applicant>? Applicant { get; set; } // List of Created Applicants from DB
+        public int ApplicantId { get; set; }
+        public IEnumerable<Applicant>? Applicants { get; set; } // List of Created Applicants from DB
 
         [Required]
         [Display(Name = "Program")]
         public int SelectedProgramId { get; set; }
         public IEnumerable<AssistanceProgram>? Programs { get; set; } // List of programs from DB
 
-        [Required]
-        [Display(Name = "Sex")]
-        public int SelectedSexId { get; set; }
-        public IEnumerable<Sex>? Sexes { get; set; } // List of sex options from DB
-
-        [Required]
-        [Display(Name = "Marital Status")]
-        public int SelectedMaritalStatusId { get; set; }
-        public IEnumerable<MaritalStatus>? MaritalStatuses { get; set; } // List of marital statuses from DB
-
         [Display(Name = "Status")]
         public int statusId { get; set; }
         public IEnumerable<Status>? Statuses { get; set; }
+
+        public Applicant? Applicant { get; set; }
+
+        public AssistanceProgram? Program { get; set; }
+
+        [Display(Name = "Decleration Date")]
+        public DateTime DeclarationDate { get; set; }
     }
 }
