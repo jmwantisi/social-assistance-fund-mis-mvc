6355b6f9d2def6fcdc50a9e07c6d58108e6d10ed
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-06 23:20:24 +0200

    refactor

--- Services/SexService.cs
+++ Services/SexService.cs
@@ -5,7 +5,16 @@ using System.Linq;
 
 namespace socialAssistanceFundMIS.Services
 {
-    public class SexService
+    public interface ISexService
+    {
+        Task<SexDTO> CreateSexAsync(SexDTO sexDto);
+        Task<SexDTO?> GetSexByIdAsync(int id);
+        Task<List<SexDTO>> GetAllSexesAsync();
+        Task<SexDTO> UpdateSexAsync(int id, SexDTO updatedSexDto);
+        Task<bool> DeleteSexAsync(int id);
+        Task<bool> PermanentlyDeleteSexAsync(int id);
+    }
+    public class SexService : ISexService
     {
         private readonly ApplicationDbContext _context;
 
