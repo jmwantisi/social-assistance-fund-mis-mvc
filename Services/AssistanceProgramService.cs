52a49ee7ae05dd8ffbd018370eab2de34e6384e9
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-06 23:20:59 +0200

    refactor

--- Services/AssistanceProgramService.cs
+++ Services/AssistanceProgramService.cs
@@ -4,7 +4,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace socialAssistanceFundMIS.Services
 {
-    public class AssistanceProgramService
+    public interface IAssistanceProgramService
+    {
+        Task<AssistanceProgramDTO> CreateAssistanceProgramAsync(AssistanceProgramDTO dto);
+        Task<AssistanceProgramDTO?> GetAssistanceProgramByIdAsync(int id);
+        Task<List<AssistanceProgramDTO>> GetAllAssistanceProgramsAsync();
+        Task<AssistanceProgramDTO> UpdateAssistanceProgramAsync(int id, AssistanceProgramDTO dto);
+        Task<bool> DeleteAssistanceProgramAsync(int id);
+        Task<bool> PermanentlyDeleteAssistanceProgramAsync(int id);
+    }
+
+    public class AssistanceProgramService : IAssistanceProgramService
     {
         private readonly ApplicationDbContext _context;
 
