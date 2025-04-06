a29e26a05f9d33b1d21f92289c598d88afc98063
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-06 23:20:33 +0200

    refactor

--- Services/PhoneNumberTypeService.cs
+++ Services/PhoneNumberTypeService.cs
@@ -4,7 +4,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace socialAssistanceFundMIS.Services
 {
-    public class PhoneNumberTypeService
+    public interface IPhoneNumberTypeService
+    {
+        Task<PhoneNumberTypeDTO> CreatePhoneNumberTypeAsync(PhoneNumberTypeDTO dto);
+        Task<PhoneNumberTypeDTO?> GetPhoneNumberTypeByIdAsync(int id);
+        Task<List<PhoneNumberTypeDTO>> GetAllPhoneNumberTypesAsync();
+        Task<PhoneNumberTypeDTO> UpdatePhoneNumberTypeAsync(int id, PhoneNumberTypeDTO dto);
+        Task<bool> DeletePhoneNumberTypeAsync(int id);
+        Task<bool> PermanentlyDeletePhoneNumberTypeAsync(int id);
+    }
+    public class PhoneNumberTypeService : IPhoneNumberTypeService
     {
         private readonly ApplicationDbContext _context;
 
