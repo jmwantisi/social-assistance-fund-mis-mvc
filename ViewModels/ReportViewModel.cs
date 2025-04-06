89f2ecda922db0b7340aa271d89eb456b63aba5c
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-07 00:02:30 +0200

    add report view model

---
+++ ViewModels/ReportViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialAssistanceFundMisMcv.ViewModels
+{
+    public class ReportViewModel
+    {
+        public int[] StatusData { get; set; } = new int[2]; // Pending, Approved
+        public int[] GenderData { get; set; } = new int[2]; // Male, Female
+        public List<string> ProgramLabels { get; set; } = new();
+        public List<int> ProgramData { get; set; } = new();
+    }
+
+}
