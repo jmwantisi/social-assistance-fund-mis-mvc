8485948c287d83e19c449527806c57d07a24db99
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-06 17:15:24 +0200

    date formatting

--- ViewModels/ApplicationViewModel.cs
+++ ViewModels/ApplicationViewModel.cs
@@ -10,7 +10,7 @@ namespace SocialAssistanceFundMisMcv.ViewModels
 
         [Required]
         [Display(Name = "Application Date")]
-        public DateTime ApplicationDate { get; set; }
+        public DateOnly ApplicationDate { get; set; } = DateOnly.FromDateTime(DateTime.Today);
 
         public string? ApplicantFullName { get; set; }
 
@@ -25,14 +25,32 @@ namespace SocialAssistanceFundMisMcv.ViewModels
         public IEnumerable<AssistanceProgram>? Programs { get; set; } // List of programs from DB
 
         [Display(Name = "Status")]
-        public int statusId { get; set; }
+        public int StatusId { get; set; }
         public IEnumerable<Status>? Statuses { get; set; }
 
         public Applicant? Applicant { get; set; }
 
+        public int ProgramId { get; set; }
+
         public AssistanceProgram? Program { get; set; }
 
-        [Display(Name = "Decleration Date")]
-        public DateTime DeclarationDate { get; set; }
+        [Display(Name = "Declaration Date")]
+        public DateOnly? DeclarationDate { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+
+        public string FormattedDeclarationDate =>
+            DeclarationDate.HasValue ? GetFormattedDate(DeclarationDate.Value) : string.Empty;
+
+        private string GetFormattedDate(DateOnly date)
+        {
+            string daySuffix = GetDaySuffix(date.Day);
+            return $"{date.Day}{daySuffix}, {date.ToString("MMMM")}, {date.Year}";
+        }
+
+        private string GetDaySuffix(int day)
+        {
+            return (day % 10 == 1 && day != 11) ? "st" :
+                   (day % 10 == 2 && day != 12) ? "nd" :
+                   (day % 10 == 3 && day != 13) ? "rd" : "th";
+        }
     }
 }
