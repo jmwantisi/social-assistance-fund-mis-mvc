422062a7f19b181763d75297a6556c54ac631140
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-06 23:21:14 +0200

    refactor

--- Services/GeographicLocationService.cs
+++ Services/GeographicLocationService.cs
@@ -7,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace socialAssistanceFundMIS.Services
 {
-    public class GeographicLocationService
+    public interface IGeographicLocationService
+    {
+        Task<List<GeographicLocation>> GetVillagesWithHierarchyAsync();
+        Task<string> GetVillageHierarchyByIdAsync(int? villageId);
+        Task<GeographicLocation> CreateGeographicLocationAsync(GeographicLocation location);
+        Task<GeographicLocation?> GetGeographicLocationByIdAsync(int id);
+        Task<List<GeographicLocation>> GetAllGeographicLocationsAsync();
+        Task<GeographicLocation> UpdateGeographicLocationAsync(int id, GeographicLocation location);
+        Task DeleteGeographicLocationAsync(int id);
+        Task PermanentlyDeleteGeographicLocationAsync(int id);
+    }
+    public class GeographicLocationService : IGeographicLocationService
     {
         private readonly ApplicationDbContext _context;
 
