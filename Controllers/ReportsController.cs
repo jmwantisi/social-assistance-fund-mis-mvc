848cd699a29be38695b1619f1aed27959e5de881
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-07 00:03:06 +0200

    add report controller

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialAssistanceFundMisMcv.Services;
+using SocialAssistanceFundMisMcv.ViewModels;
+
+namespace SocialAssistanceFundMisMcv.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly ReportService _reportService;
+
+        public ReportsController(ReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var (pending, approved) = await _reportService.GetApplicationStatusCountsAsync();
+            var (male, female) = await _reportService.GetApprovedApplicantsByGenderAsync();
+            var perProgram = await _reportService.GetApplicationCountsPerProgramAsync();
+
+            var viewModel = new ReportViewModel
+            {
+                StatusData = new[] { pending, approved },
+                GenderData = new[] { male, female },
+                ProgramLabels = perProgram.Keys.ToList(),
+                ProgramData = perProgram.Values.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
