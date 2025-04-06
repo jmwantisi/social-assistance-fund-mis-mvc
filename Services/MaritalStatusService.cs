6da224eab8dec1d4fbaa77548d3ee51ae7365851
James Mwantisi <37237803+jmwantisi@users.noreply.github.com>
2025-04-06 23:20:50 +0200

    refactor

--- Services/MaritalStatusService.cs
+++ Services/MaritalStatusService.cs
@@ -4,7 +4,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace socialAssistanceFundMIS.Services
 {
-    public class MaritalStatusService
+    public interface IMaritalStatusService
+    {
+        Task<MaritalStatusDTO> CreateMaritalStatusAsync(MaritalStatusDTO maritalStatusDto);
+        Task<MaritalStatusDTO?> GetMaritalStatusByIdAsync(int id);
+        Task<List<MaritalStatusDTO>> GetAllMaritalStatusesAsync();
+        Task<MaritalStatusDTO> UpdateMaritalStatusAsync(int id, MaritalStatusDTO updatedMaritalStatusDto);
+        Task<bool> DeleteMaritalStatusAsync(int id);
+        Task<bool> PermanentlyDeleteMaritalStatusAsync(int id);
+    }
+    public class MaritalStatusService : IMaritalStatusService
     {
         private readonly ApplicationDbContext _context;
 
